ea6113e35e4d8e518ee600edfbcf4ba9603736ab
Bharadwaj Kota <96335728+Bharat234@users.noreply.github.com>
2025-03-12 17:04:33 +0530

    Delete Laser-Game directory

--- Laser-Game/config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    GPIO_CHIP = "/dev/gpiochip0"  # Check using `gpiodetect`
-    IR_PIN = 14  # Change based on wiring
-    BUZZER_PIN = 15
-    LED_PIN = 16
-
-    MQTT_BROKER = "192.168.0.101"  # Change to your MQTT broker’s IP
-    MQTT_PORT = 1883
-    MQTT_TOPIC_HIT = "laser_game/hit"
